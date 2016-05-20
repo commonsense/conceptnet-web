0b5cff71a7fc4beb45a051e6ed8060fc3359be72
Rob Speer <rob@luminoso.com>
2016-05-20 15:32:46 -0400

    use plain setup.py; pbr doesn't seem ready yet

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages, Command
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+import sys
 
-from setuptools import setup
+packages = find_packages()
+version_str = '5.5.0'
+
+if sys.version_info.major < 3:
+    print("The ConceptNet 5 code can only run in Python 3.")
+    sys.exit(1)
+
 
 setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    pbr=True,
+    name = 'conceptnet-web',
+    version = version_str,
+    description = 'Runs the Web site and API for ConceptNet',
+    author = "Rob Speer",
+    author_email = 'rob@luminoso.com',
+    packages=packages,
+    include_package_data=True,
+    install_requires=[
+        'limits', 'flask', 'flask-cors', 'flask-limiter', 'langcodes'
+    ],
+    license = 'Apache License 2.0',
 )
